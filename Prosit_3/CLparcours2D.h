706b42a4e43c15f20c99ab1a5f537f7fc4aa8ace
AlexCouCesi <150126263+AlexCouCesi@users.noreply.github.com>
2023-11-21 10:36:44 +0100

    Update CLparcours2D.h

--- Prosit_3/CLparcours2D.h
+++ Prosit_3/CLparcours2D.h
@@ -2,32 +2,24 @@
 #include "CLParcours.h"
 
 class CLparcours2D : public CLParcours {
+	using CLParcours::CLParcours;
 public:
-	CLparcours2D();
-	CLparcours2D(int);
-	void ajouterPoint(CLpoint*) override;
-	double calculDistance()override;
+	double calculDistance() override;
 	void message() override;
 };
 
-CLparcours2D::CLparcours2D() : CLParcours(){}
-
-CLparcours2D::CLparcours2D(int new_num) : CLParcours(new_num) {}
-
-void CLparcours2D::ajouterPoint(CLpoint* new_point) {
-	CLParcours::ajouterPoint(new_point);
-}
-
 double CLparcours2D::calculDistance(){
-	double distance = 0.0;
-	for (size_t i = 1; i < points.size(); i++) {
-		double dx = points[i]->x - points[i - 1]->x;
-		double dy = points[i]->y - points[i - 1]->y;
-		distance += sqrt(dx * dx + dy * dy);
+	double distance = 0;
+	CLpoint* pt = this->points[0];
+	for (int i = 1; i < this->points.size(); i++) {
+		double dx = this->points[i]->getX() - pt->getX();
+		double dy = this->points[i]->getY() - pt->getY();
+		distance += sqrt(dx*dx+dy*dy);
+		pt = this->points[i];
 	}
 	return distance;
 }
 
 void CLparcours2D::message() {
-	cout << "Calcul d'un parcours de type 2D : " << endl;
+	return this->CLParcours::message(2);
 }
