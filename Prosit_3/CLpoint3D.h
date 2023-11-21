5f0bf3f74986cb2a81f11eb3180a46918c5fa691
AlexCouCesi <150126263+AlexCouCesi@users.noreply.github.com>
2023-11-21 10:37:54 +0100

    Update CLpoint3D.h

--- Prosit_3/CLpoint3D.h
+++ Prosit_3/CLpoint3D.h
@@ -2,14 +2,20 @@
 #include "CLpoint.h"
 
 class CLpoint3D : public CLpoint {
-public:
 	double z;
+public:
 	CLpoint3D(double, double, double);
-	void afficherCoordo() const override;
+	void afficherCoordo() override;
+	double getZ();
 };
 
 CLpoint3D::CLpoint3D(double new_x, double new_y, double new_z): CLpoint(new_x, new_y), z(new_z){}
 
-void CLpoint3D::afficherCoordo() const {
-	cout << "(" << x << ";" << y << ";" << z << ")" << endl;
+void CLpoint3D::afficherCoordo() {
+	this->CLpoint::afficherCoordo();
+	cout << "(" << this->z << ")";
+}
+
+double CLpoint3D::getZ() {
+	return this->z;
 }
