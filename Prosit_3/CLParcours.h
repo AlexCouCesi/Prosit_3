5c8a852bdd5ba6028bf5baab681df33fbfebd0fb
AlexCouCesi <150126263+AlexCouCesi@users.noreply.github.com>
2023-11-21 10:36:12 +0100

    Update CLParcours.h

--- Prosit_3/CLParcours.h
+++ Prosit_3/CLParcours.h
@@ -7,27 +7,24 @@
 using namespace std;
 
 class CLParcours
-{public :
+{protected:
 	vector <CLpoint*> points;
-	vector <CLpoint3D*> points3D;
-	CLParcours();
+	void message(uint16_t dim);
+public:
 	CLParcours(int);
 	virtual void ajouterPoint(CLpoint*);
-	virtual void ajouterPoint(CLpoint3D*);
 	virtual double calculDistance() = 0;
 	virtual void message() = 0;
 };
 
-CLParcours::CLParcours() {
-	points.reserve(0);
-}
+CLParcours::CLParcours(int) : points(vector<CLpoint*>()){}
 
-CLParcours::CLParcours(int num) {
-	points.reserve(num);
+void CLParcours::message(uint16_t dim) {
+	cout << "Calcul d'un parcours de type" << dim << "D :\n";
 }
 
 void CLParcours::ajouterPoint(CLpoint* new_point) {
-	points.push_back(new_point);
+	this->points.push_back(new_point);
 }
 
 void CLParcours::ajouterPoint(CLpoint3D* new_point) {
