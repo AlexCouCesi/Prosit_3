6266b95af3b8545fab2d1e59df7860d653264669
AlexCouCesi <150126263+AlexCouCesi@users.noreply.github.com>
2023-11-21 10:37:08 +0100

    Update CLparcours3D.h

--- Prosit_3/CLparcours3D.h
+++ Prosit_3/CLparcours3D.h
@@ -2,37 +2,32 @@
 #include "CLParcours.h"
 
 class CLparcours3D : public CLParcours {
+	using CLParcours::CLParcours;
 public:
-	CLparcours3D();
-	CLparcours3D(int);
-	void ajouterPoint(CLpoint3D*) override;
 	double calculDistance() override;
 	void message() override;
 };
 
-CLparcours3D::CLparcours3D(){
-	points3D.reserve(0);
-}
-
-CLparcours3D::CLparcours3D(int new_num){
-	points3D.reserve(new_num);
-}
-
-void CLparcours3D::ajouterPoint(CLpoint3D* new_point){
-	CLParcours::ajouterPoint(new_point);
-}
-
 double CLparcours3D::calculDistance() {
-	double distance = 0.0;
-	for (size_t i = 1; i < points3D.size(); i++) {
-		double dx = points3D[i]->x - points3D[i - 1]->x;
-		double dy = points3D[i]->y - points3D[i - 1]->y;
-		double dz = points3D[i]->z - points3D[i - 1]->z;
-		distance += sqrt(dx * dx + dy * dy + dz * dz);
+	double distance = 0;
+	CLpoint* pt = this->points[0];
+	for (int i = 1; i < this->points.size(); i++) {
+		double dx = this->points[i]->getX() - pt->getX();
+		double dy = this->points[i]->getY() - pt->getY();
+		if (CLpoint3D* p3d = dynamic_cast<CLpoint3D*>(this->points[i])) {
+			if (CLpoint3D* pt3d = dynamic_cast<CLpoint3D*>(pt)) {
+				double dz = p3d->getZ() - pt3d->getZ();
+				distance += sqrt(dx * dx + dy * dy + dz * dz);
+			}
+		}
+		else {
+			distance += sqrt(dx * dx + dy * dy);
+		}
+		pt = this->points[i];
 	}
 	return distance;
 }
 
 void CLparcours3D::message() {
-	cout << "Calcul d'un parcours de type 3D :" << endl;
+	return this->CLParcours::message(3);
 }
